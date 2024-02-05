51bf330e6bb89a4a0b1b214f5ae88ba6af517920
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-02-05 19:54:29 +0900

    [Design] 로그인/회원가입 button으로 변경

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,42 +8,65 @@ let Nav = () => {
     return (
         <nav>
             <div>
-            <span className="main">
-                <NavLink to="/">
-                    Main(나중에 로고로 변경)
-                </NavLink>
-            </span>
-            
-            <span>
-                <NavLink to="/puppyNote">
-                    알림장
-                </NavLink>
-            </span>
-            <span>
-                <NavLink to="/gallery">
-                    사진첩
-                </NavLink>
-            </span>
-            <span>
-                <NavLink to="/board">
-                    게시판
-                </NavLink>
-            </span>
-            <span>
-                <NavLink to="/follow">
-                    친구목록
-                </NavLink>
-            </span>
-            <span>
-                <NavLink to="/login">
-                    로그인
-                </NavLink>
-            </span>
-            <span>
-                <NavLink to="/register">
-                    회원가입
-                </NavLink>
-            </span>
+                
+                <span>
+                    <NavLink to="/">
+                         Main(로고)
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/puppyNote">
+                        알림장
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/gallery">
+                        사진첩
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/board">
+                        게시판
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/follow">
+                        친구목록
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/login">
+                        <button id="loginBut">
+                            로그인
+                        </button>
+                    </NavLink>
+                </span>
+                <span>
+                    <NavLink to="/register">
+                        <button id="registerBut">
+                            회원가입
+                        </button>
+                    </NavLink>
+                </span>
+                
+                {/* 추후 수정 : 로그인 / 로그아웃 케이스 나누어서 버튼 보여주기
+                로그인 => 마이페이지, 로그아웃
+                로그아웃 => 로그인, 회원가입 */}
+
+                {/* <span>
+                    <NavLink to="/mypage">
+                        <button>
+                            마이페이지
+                        </button>
+                    </NavLink>
+                </span>
+                <span className="nav-link">
+                    <NavLink to="/logout">
+                        <button>
+                            로그아웃
+                        </button>
+                    </NavLink>
+                </span> */}
             </div>
         </nav>
     )
