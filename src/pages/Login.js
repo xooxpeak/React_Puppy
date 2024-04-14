01f19d6cc08bc79f1aa5546fcc12cac0590129c4
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-04-14 15:00:00 +0900

    [Add] accessToken을 쿠키에 저장

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,7 +40,16 @@ let Login = () => {
                 // 로그인 성공
                 console.log(res.data)
                 if (res.status === 200) {
-                    setCookies('user', { userId, password });  // 쿠키에 사용자 정보 저장
+                    // 쿠키에 사용자 정보 저장
+                    // setCookies('user', { userId, password });  
+
+                    // 서버로부터 받은 액세스 토큰을 쿠키에 저장
+                    setCookies('accessToken', res.data.accessToken);
+
+                    // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
+
+
                     alert("로그인 성공!");
                     navigate("/");  // 메인페이지로 이동
                     
