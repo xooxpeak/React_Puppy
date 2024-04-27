0dab0c904afbae3625078703fa3461582f942341
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-04-27 18:00:00 +0900

    [Feat] 로그인 여부에 따른 네비바 변환

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useCookies } from "react-cookie";
 
 function CollapsibleExample() {
+
+  let [cookies, setCookies, removeCookies] = useCookies(['accessToken']);  // 쿠키 관리
+  let [isLogin, setIsLogin] = useState(false); // 로그인 상태 관리
+  
+  useEffect(() => {
+    // 쿠키에서 accessToken을 확인하여 로그인 상태를 설정
+    if (cookies.accessToken) {
+        setIsLogin(true); // 로그인 상태로 설정
+    } else {
+        setIsLogin(false); // 로그아웃 상태로 설정
+    }
+  }, [cookies.accessToken]);
+
+
+    // 로그아웃 함수
+    const handleLogout = () => {
+      // 쿠키에서 accessToken 제거
+      removeCookies('accessToken');
+      // 로그인 상태 변경
+      setIsLogin(false);
+  };
+
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
       <Container>
@@ -27,8 +52,18 @@ function CollapsibleExample() {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="/login">로그인</Nav.Link>
-            <Nav.Link href="/register">회원가입</Nav.Link>
+            {/* 로그인 상태에 따라 다른 링크를 표시 */}
+            {isLogin ? (
+              <>
+                <Nav.Link href="/main">마이페이지</Nav.Link>
+                <Nav.Link onClick={handleLogout}>로그아웃</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login">로그인</Nav.Link>
+                <Nav.Link href="/register">회원가입</Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
