3d47d223965af9c3cbec7601672da23b032bb1c0
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-05-27 12:51:35 +0900

    [Modify] 여러 개의 이미지를 업로드할수 있도록 수정

--- src/pages/CreateGallery.js
+++ src/pages/CreateGallery.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import Nav2 from "../components/Nav2";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,24 +10,47 @@ import '../css/CreateGallery.css'
 
 let CreateGallery = () => {
 
-    let [cookies, setCookies, removeCookies] = useCookies(['accessToken']);
+    let [cookies] = useCookies(['accessToken']);
     let [files, setFiles] = useState([]);
     let [selectedDate, setSelectedDate] = useState(null);
-    let [imagePreview, setImagePreview] = useState(null); // 이미지 미리보기를 위한 상태 변수
+    // let [imagePreview, setImagePreview] = useState([null]); // 이미지 미리보기를 위한 상태 변수
+    let [imagePreviews, setImagePreviews] = useState([]); //  여러 이미지 미리보기를 위한 상태 변수
+    let navigate = useNavigate(); // useNavigate 훅 사용
 
     // 파일 선택 시 호출되는 함수
-    let uploadOnChange = (e) => {
-        setFiles(Array.from(e.target.files))  // 선택된 파일 목록을 상태 변수 files에 저장함
+    // let uploadOnChange = (e) => {
+    //     setFiles(Array.from(e.target.files))  // 선택된 파일 목록을 상태 변수 files에 저장함
     
-            // 선택된 파일 중 첫 번째 파일의 미리보기를 표시함
-            if (e.target.files.length > 0) {
-                const reader = new FileReader();
-                reader.onload = (e) => {
-                    setImagePreview(e.target.result);
+    //         // 선택된 파일 중 첫 번째 파일의 미리보기를 표시함
+    //         if (e.target.files.length > 0) {
+    //             const reader = new FileReader();
+    //             reader.onload = (e) => {
+    //                 setImagePreview(e.target.result);
+    //             };
+    //             reader.readAsDataURL(e.target.files[0]);
+    //         }
+    // }
+
+    // 여러 파일 선택 시 호출되는 함수
+    let uploadOnChange = (e) => {
+        let selectedFiles = Array.from(e.target.files);
+        setFiles(selectedFiles);  // 선택된 파일 목록을 상태 변수 files에 저장함
+
+        // 선택된 파일들의 미리보기를 표시함
+        let previews = selectedFiles.map((file) => {
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            return new Promise((resolve) => {
+                reader.onload = () => {
+                    resolve(reader.result);
                 };
-                reader.readAsDataURL(e.target.files[0]);
-            }
-    }
+            });
+        });
+
+        Promise.all(previews).then((images) => {
+            setImagePreviews(images);
+        });
+    };    
 
     // 날짜 선택 시 호출되는 함수
     let handleDateChange = (date) => {
@@ -58,6 +82,8 @@ let CreateGallery = () => {
           .then((res) => {
               console.log(res.data);
               alert("이미지 업로드 성공!");
+              navigate("/gallery");  // 사진첩 목록으로 이동
+
           }).catch((err) => {
               console.error(err);
               alert("이미지 업로드 실패");
@@ -69,8 +95,11 @@ let CreateGallery = () => {
         <div>
             <Nav2/>
         </div>
-            <form>
-                <h4><strong>사진첩 작성</strong></h4>
+
+        <h3>📷사진첩 작성</h3>
+        <div className="CreateGallery-wrapper">
+            <form className="CreateGallery-container">
+                {/* <h4><strong>사진첩 작성</strong></h4> */}
                 <DatePicker
                     selected={selectedDate}
                     onChange={handleDateChange}
@@ -79,11 +108,15 @@ let CreateGallery = () => {
                 />
                 <hr></hr>
                 {/* 이미지 미리보기 */}
-                {imagePreview && <img src={imagePreview} alt="Uploaded" style={{ width: "300px", height: "150px" }} />}
+               {/* {imagePreview && <img src={imagePreview} alt="Uploaded" style={{ width: "300px", height: "150px" }} />} */}
+               {imagePreviews.map((preview, index) => (
+                            <img key={index} src={preview} alt={`Uploaded ${index}`} style={{ width: "150px", height: "100px", margin: "5px" }} />
+                        ))}
                 <hr></hr>
-                <input type="file" accept="image/*" onChange={uploadOnChange}/>
+                <input type="file" accept="image/*" multiple onChange={uploadOnChange}/>
                 <button label="이미지 업로드" onClick={uploadFiles}>작성하기</button>
             </form>
+            </div>
         </>
         );
 };
