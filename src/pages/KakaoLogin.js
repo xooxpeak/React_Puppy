976976e704943d3dbf72ff23142ce466e42339d7
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-06-12 23:10:56 +0900

    [Feat] 카카오 로그인 구현 -> 완료

--- src/pages/KakaoLogin.js
+++ src/pages/KakaoLogin.js
@@ -2,26 +2,28 @@ import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 let KakaoLogin = () => {
     let [cookies, setCookies] = useCookies(['accessToken']);
-
     // 현재 URL에 대한 정보를 가져오는 훅
     let location = useLocation();
+    let navigate = useNavigate();
 
     // location.search를 통해 현재 URL의 쿼리 파라미터를 가져와 URLSearchParams 객체를 생성하고
     // 그 중 code 파라미터를 추출
     const params = new URLSearchParams(location.search);
     let code = params.get('code');
 
-    // const code = new URL(window.location.href).serchParams.get("code");
-
-
     // useEffect Hook
     useEffect(() => {
-        console.log(code);
-    })
+        if (code) {
+            console.log("code:", code);
+            login();  // 코드가 존재하면 로그인 함수 호출
+        } else {
+            console.log("code not found");
+        }
+    }, [code]);
 
 
     // login 함수
@@ -29,35 +31,41 @@ let KakaoLogin = () => {
     let login = () => {
         axios({
             url: 'http://localhost:8082/api/v1/auth/n/kakaoLogin',
-            method: 'GET',
+            method: 'POST',
             data: {
                 code: code
             },
-            headers: {
-                'Authorization': 'Bearer ' + cookies.accessToken
-             }
         })
             .then((res) => {
-                console.log(res);
-                // 계속 쓸 정보들(이름) 등은 localStorage에 저장해두기
-                // localStorage.setItem("name", res.data.);
-                
-                // 로그인이 성공하면 이동
-                // Navigate("/"); 
+                // 인증 토큰을 받아서 처리
+                console.log("res:",res);
+
+                setCookies('accessToken', res.data.accessToken, {
+                    path: '/',
+                    secure: true,
+                    sameSite: 'strict',
+                    maxAge: 7 * 24 * 60 * 60 // 쿠키 만료 시간 설정 (7일)
+                });
+
+                // 토큰을 localStorage에 저장(?)
+                // localStorage.setItem("accessToken: ", res.data.accessToken);
+
+                // 로그인이 성공하면 메인 페이지로 이동
+                navigate('/'); 
             })
             .catch((error) => {
                 console.log("Error: ", error);
+                alert("로그인에 실패하였습니다.")
+                navigate('/login');
             });
-
 
     };
 
 
-
     return (
         <>
-        <p>로그인 중입니다.</p>
-        <p>잠시만 기다려주세요.</p>
+            <p>로그인 중입니다.</p>
+            <p>잠시만 기다려주세요.</p>
         </>
         );
 };
