055d20da1cd2764ebfa990ca4f91c32475525849
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-07-02 23:33:18 +0900

    [Feat] 유치원 등록 구현 -> 완료

--- src/pages/CreateStore.js
+++ src/pages/CreateStore.js
@@ -3,35 +3,48 @@ import '../css/CreateStore.css';
 import Nav2 from "../components/Nav2";
 import { useState } from "react";
 import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
-// useState 훅을 사용하여 객체 형태의 store 상태 선언
 let CreateStore = () => {
-    let [store,setStore] = useState({
-        storeName : "",
-        managerName : "",
-        storePhone : "",
-        storeAddr1 : "",
-        storeAddr2 : "",
+
+    let [cookies] = useCookies(['accessToken']);
+    let navigate = useNavigate();
+
+    let [store, setStore] = useState({
+        storeName: "",
+        managerName: "",
+        storePhone: "",
+        storeAddr1: "",
+        storeAddr2: "",
     });
 
-    // axios를 사용하여 HTTP POST 요청을 보냄
-    let createStore = () => {
-        axios.post('http://localhost:8082/api/v1/auth/y/store', {
-            params:{
-                storeName : store.storeName,
-                managerName : store.managerName,
-                storePhone : store.storePhone,
-                storeAddr1 : store.storeAddr1,
-                storeAddr2 : store.storeAddr2
+    let createStore = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:8082/api/v1/auth/y/saveStore', {
+            storeName: store.storeName,
+            managerName: store.managerName,
+            storePhone: store.storePhone, 
+            storeAddr1: store.storeAddr1,
+            storeAddr2: store.storeAddr2
+        },            
+        {
+            headers: {
+                'Authorization': 'Bearer ' + cookies.accessToken,
+                'Content-Type': 'application/json' // Ensure JSON content type
             }
         }).then((res) => {
+            alert("유치원 등록 성공!");
             console.log(res);
+            navigate('/');
+        }).catch((error) => {
+            console.error(error);
         });
     }
 
-    // useState 훅으로 관리되는 store 객체의 속성을 업데이트하는 함수
     let onChangeStoreData = (e) => {
-        setStore({...store, [e.target.name] : e.target.value})
+        setStore({ ...store, [e.target.name]: e.target.value });
         console.log(store);
     }
 
@@ -40,39 +53,32 @@ let CreateStore = () => {
             <div>
                 <Nav2 />
             </div>
-
-            <form className="CreateStoreForm">
+            <div>
+                <h3 style={{ textAlign: 'center', marginTop: '20px' }}><strong>유치원 등록</strong></h3>
+            </div>
+            <form className="CreateStoreForm" onSubmit={createStore}>
                 <div>
-                    <h2 id='createStore_title'> 유치원 신청 </h2>
-                </div>
-
-                {/* 유치원 이름 */}
-                <div>
-                    <input type='text' className="input-store" name={"storeName"} onChange={onChangeStoreData} placeholder='유치원 이름' autoFocus/>
-                </div>
-
-                {/* 관리자 이름 */}
-                <div>
-                    <input type='text' className="input-store" name={"managerName"} onChange={onChangeStoreData} placeholder="관리자 이름"/>
-                </div>
-
-                {/* 유치원 전화번호 */}
-                <div>
-                    <input type='text' className="input-store" name={"storePhone"} onChange={onChangeStoreData} placeholder="유치원 전화번호"/>
-                </div>
-
-                {/* 주소1 */}
-                <div>
-                    <input type='text' className="input-store" name={"storeAddr1"} onChange={onChangeStoreData} placeholder="주소"/>
-                </div>
-
-                {/* 주소2 (상세주소) */}
-                <div>
-                    <input type='text' className="input-store" name={"storeAddr2"} onChange={onChangeStoreData} placeholder="상세주소"/>
+                    <input type='text' className="input-store" name="storeName" onChange={onChangeStoreData} placeholder='유치원 이름' autoFocus />
                 </div>
 
                 <div>
-                    <button type="submit" id="createStoreBut" onClick={createStore}>신청하기&nbsp;</button>
+                    <input type='text' className="input-store" name="managerName" onChange={onChangeStoreData} placeholder="관리자 이름" />
+                </div>
+
+                <div>
+                    <input type='text' className="input-store" name="storePhone" onChange={onChangeStoreData} placeholder="유치원 전화번호" />
+                </div>
+
+                <div>
+                    <input type='text' className="input-store" name="storeAddr1" onChange={onChangeStoreData} placeholder="주소" />
+                </div>
+
+                <div>
+                    <input type='text' className="input-store" name="storeAddr2" onChange={onChangeStoreData} placeholder="상세주소" />
+                </div>
+
+                <div>
+                    <button type="submit" id="createStoreBut">신청하기&nbsp;</button>
                 </div>
             </form>
         </div>
@@ -80,5 +86,3 @@ let CreateStore = () => {
 }
 
 export default CreateStore;
-
-
